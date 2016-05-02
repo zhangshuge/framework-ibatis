f3ccfad7c129c05b5b53b364555d6cbe62a7de06
zhangshuge <zc_ciyuan@163.com>
2016-05-02 15:59:57 +0800

    Change-Id: If04095526bf51d79d9f58cad63f3b9b3e7a917cc Signed-off-by: zhangshuge <zc_ciyuan@163.com>

--- src/main/java/com/ccbtrust/ibatis/support/EntityDao.java
+++ src/main/java/com/ccbtrust/ibatis/support/EntityDao.java
@@ -1,4 +1,11 @@
 package com.ccbtrust.ibatis.support;
+
+import java.io.Serializable;
+import java.util.List;
+
+import com.ccbtrust.ibatis.exception.DataAccessException;
+import com.ccbtrust.ibatis.page.PageBean;
+
 /**
  * 实体访问接口
  * @author ciyuan
@@ -6,5 +13,78 @@ package com.ccbtrust.ibatis.support;
  * @param <T>
  */
 public interface EntityDao<T> {
+	/**
+	 * 无分页机制查询  Find
+	 * @param obj
+	 * @return
+	 * @throws Exception
+	 */
+	List<T> findBy(Object obj) throws DataAccessException;
+
+	/**
+	 * 分页机制查询  Find
+	 * @param pagebean
+	 * @param obj
+	 * @throws Exception
+	 */
+	void pagedFindBy(PageBean pageBean, Object obj)throws DataAccessException;
 	
+	/**
+	 * 分页机制查询  Find
+	 * @param pagebean
+	 * @param obj
+	 * @throws Exception
+	 */
+	void pagedFindBy(String statementName, String countStatementName, PageBean pageObj, Object obj)throws DataAccessException;
+
+	/**
+	 * 单条新增  Insert
+	 *  <selectKey resultClass="int" type="pre" keyProperty="roleCode" > 
+	 * 		SELECT seq_cm_temp_id.NEXTVAL AS VALUE FROM DUAL 
+	 *  </selectKey> 
+	 * 如果使用了上述 标签  可以返回序列的value  否则返回null
+	 * @param obj
+	 * @throws Exception
+	 */
+	Object insert(Object obj)throws DataAccessException;
+	/**
+	 * 单条更改 Update
+	 * @param obj
+	 * @return
+	 * @throws Exception
+	 */
+	int update(Object obj)throws DataAccessException;
+	/**
+	 * 按主键查询  Get
+	 * @param obj
+	 * @return
+	 * @throws Exception
+	 */
+	T getByKey(Serializable obj)throws DataAccessException;
+	
+	/**
+	 * 删除
+	 * @param t
+	 * @return
+	 * @throws DataAccessException
+	 */
+	int delete(Object t) throws DataAccessException;
+	
+	/**
+	 * 开启事务
+	 * @throws DataAccessException
+	 */
+	public void txBegin() throws DataAccessException;
+
+	/**
+	 * 提交事务
+	 * @throws DataAccessException
+	 */
+	public void txCommit() throws DataAccessException ;
+
+	/**
+	 * 结束事务
+	 * @throws DataAccessException
+	 */
+	public void txEnd() throws DataAccessException;
 }
--- src/main/java/com/ccbtrust/ibatis/support/EntityDaoImpl.java
+++ src/main/java/com/ccbtrust/ibatis/support/EntityDaoImpl.java
@@ -1,15 +1,46 @@
 package com.ccbtrust.ibatis.support;
 
+import java.io.Serializable;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 
 import com.ccbtrust.ibatis.AbstractDao;
+import com.ccbtrust.ibatis.exception.DataAccessException;
+import com.ccbtrust.ibatis.page.PageBean;
 
 public class EntityDaoImpl<T> extends AbstractDao implements EntityDao<T> {
 
+	protected static final String POSTFIX_FIND = ".Find";
+    protected static final String POSTFIX_SELECT_PRIAMARYKEY = ".Get";
+    protected static final String POSTFIX_SELECT_PRIAMARYKEYMAP = ".GetMap";
+    protected static final String POSTFIX_INSERT = ".Insert";
+    protected static final String POSTFIX_UPDATE = ".Update";
+    protected static final String POSTFIX_DELETE = ".Delete";
+    protected Class<?> entityClass;
 	@Autowired
     private Environment environment;
-
+	public EntityDaoImpl() {
+		entityClass = getSuperClassGenricType(getClass(), 0);
+    }
+	private static Class<?> getSuperClassGenricType(Class<?> clazz, int index) {
+		Type genType = clazz.getGenericSuperclass();
+		if (!(genType instanceof ParameterizedType)) {
+		    return Object.class;
+		}
+		Type[] params = ((ParameterizedType) genType).getActualTypeArguments();
+		if (index >= params.length || index < 0) {
+		    return Object.class;
+		}
+		if (!(params[index] instanceof Class)) {
+		    return Object.class;
+		} else {
+		    return (Class<?>) params[index];
+		}
+    }
 	@Override
 	protected String getMapFile() {
 		return environment.getProperty("sql.map.config.file","SqlMapConfig.xml");
@@ -18,6 +49,59 @@ public class EntityDaoImpl<T> extends AbstractDao implements EntityDao<T> {
 	@Override
 	protected String getDbType() {
 		return environment.getProperty("db.type", DB_TYPE_ORACLE);
+	}
+
+	@Override
+	public List<T> findBy(Object obj) throws DataAccessException {
+		return getDao().find((new StringBuilder(String.valueOf(entityClass.getSimpleName()))).append(POSTFIX_FIND).toString(), obj);
+	}
+
+	@Override
+	public void pagedFindBy(PageBean pageBean, Object obj)
+			throws DataAccessException {
+		getDao().find((new StringBuilder(String.valueOf(entityClass.getSimpleName()))).append(POSTFIX_FIND).toString(), pageBean, obj);
+	}
+
+	@Override
+	public void pagedFindBy(String statementName, String countStatementName,
+			PageBean pageObj, Object obj) throws DataAccessException {
+		getDao().find(statementName, countStatementName, pageObj, obj);
+	}
+
+	@Override
+	public Object insert(Object obj) throws DataAccessException {
+		return getDao().save((new StringBuilder(String.valueOf(entityClass.getSimpleName()))).append(POSTFIX_INSERT).toString(), obj);
+	}
+
+	@Override
+	public int update(Object obj) throws DataAccessException {
+		return getDao().update((new StringBuilder(String.valueOf(entityClass.getSimpleName()))).append(POSTFIX_UPDATE).toString(), obj);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public T getByKey(Serializable obj) throws DataAccessException {
+		return (T) getDao().get((new StringBuilder(String.valueOf(entityClass.getSimpleName()))).append(POSTFIX_SELECT_PRIAMARYKEY).toString(), obj);
+	}
+
+	@Override
+	public int delete(Object t) throws DataAccessException {
+		return getDao().delete((new StringBuilder(String.valueOf(entityClass.getSimpleName()))).append(POSTFIX_DELETE).toString(), t);
+	}
+
+	@Override
+	public void txBegin() throws DataAccessException {
+		getDao().txBegin();
+	}
+
+	@Override
+	public void txCommit() throws DataAccessException {
+		getDao().txCommit();
+	}
+
+	@Override
+	public void txEnd() throws DataAccessException {
+		getDao().txEnd();
 	}
 
 }
